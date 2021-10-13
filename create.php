100d118139f47b006ad8dbdaaae803c2fa5f6c8c
iamThiagoo <thiagofer2003@outlook.com>
2021-10-13 18:00:14 -0300

    Criada e estilizada a parte de cadastro de contato

---
+++ create.php
@@ -0,0 +1,29 @@
+<?php
+    include_once("assets/templates/header.php");
+?>
+<main class="main-content" id="create-container">
+    <h2>Cadastrar contato</h2>
+    <form action="" method="POST">
+        <div class="input">
+            <label class="sr-only" for="name">Digite o nome do contato</label>
+            <input type="text" name="name" id="name" maxlength="100" placeholder="Digite o nome do contato">
+            <div class="icon">
+                <i class="fas fa-user"></i>
+            </div>
+        </div>
+        <div class="input">
+            <label class="sr-only" for="phone">Digite o telefone do contato</label>
+            <input type="number" name="phone" id="phone" placeholder="Digite o telefone do contato">
+            <div class="icon">
+                <i class="fas fa-phone"></i>
+            </div>
+        </div>
+        <div class="textarea">
+            <textarea name="textarea" id="textarea" cols="30" rows="10" placeholder="Observações..."></textarea>
+        </div>
+        <input type="submit" class="submit-btn" value="Salvar contato">
+    </form>
+</main>
+<?php
+    include_once("assets/templates/footer.php");
+?>
